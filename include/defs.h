f4a9facc8e148f1925cd37b665385238f04f1d5a
wispl <wispl.8qbkk@slmail.me>
2026-07-15 20:55:36 -0400

    feat: common handle abstraction

--- include/defs.h
+++ include/defs.h
@@ -18,4 +18,33 @@
 #include "stm32f4xx_hal.h"
 #endif // end TEST
 
+/// Common abstraction over SPI, UART, I2C
+/// Use this handle struct when a sensor could be configured to use more than of
+/// the protocols or if the sensor uses a protocol that might be disabled, like
+/// UART or I2C. This helps isolate ifdefs to only implementation files. 
+enum protocol { SPI, UART, I2C };
+struct handle {
+	enum protocol protocol;
+	union {
+#ifdef HAL_I2C_MODULE_ENABLED
+		struct handle_i2c {
+			I2C_HandleTypeDef *handle;
+			uint32_t address;
+		} i2c;
+#endif
+		// We always have spi present, so we don't have to gate it
+		struct handle_spi {
+			SPI_HandleTypeDef *handle;
+			GPIO_TypeDef *port;
+			uint8_t pin;
+		} spi;
+#ifdef HAL_UART_MODULE_ENABLED
+		struct handle_uart {
+			UART_HandleTypeDef *handle;
+		} uart;
+#endif
+	} def;
+};
+
+
 #endif // end DEFS_H
