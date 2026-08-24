feb3eaac3251fb838e0acf3248d0736b1ed89c6b
wispl <wispl.8qbkk@slmail.me>
2026-08-24 00:24:22 -0400

    feat: auto detect hal header file
    Using a similar cmake trick to conditionally compile in peripheral code, we can
    also use it to pull in the right header file automatically. Also reorganize
    imports while we are at it.

--- include/hal.h
+++ include/hal.h
@@ -2,14 +2,18 @@
 #define HAL_H
 
 #ifndef TEST
-#ifdef USE_STM32_H7XX
-#include "stm32h7xx_hal.h"
-#elif USE_STM32_L4XX
-#include "stm32l4xx_hal.h"
+// Stringifies the macro, since #include requires a string
+// NOTE: this trick was once used in the linux kernel via __gcc_header, go check
+// it out here. Note that it is no longer in use now.
+// https://git.kernel.org/pub/scm/linux/kernel/git/torvalds/linux.git/tree/include/linux/compiler-gcc.h?id=f6d133f877c8bb0a0934dc8c521c758ee771e901#n125
+#define _hal_header(x) #x
+#define __hal_header(x) _hal_header(stm32 ## x.h)
+#define hal_header(x) __hal_header(x)
+// This will include the correct hal header, see the root CMakeLists.txt
+// for more information on how we get `HAL_STM32_SERIES`
+#include hal_header(HAL_STM32_SERIES)
 #else
-#include "stm32f4xx_hal.h"
-#endif
-#else
+// Dummy stub used for testing
 #define HAL_Delay(...) (0)
 #endif
 
